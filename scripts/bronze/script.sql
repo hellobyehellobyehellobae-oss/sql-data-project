802570d4b3965530ca4feb782c4c8fb9435e069e
hellobyehellobyehellobae-oss <hellobyehellobyehellobae@gmail.com>
2026-01-08 22:41:18 +0200

    Update script.sql

--- scripts/bronze/script.sql
+++ scripts/bronze/script.sql
@@ -8,3 +8,29 @@ CREATE TABLE bronze.crm_cust_info (
     cst_gndr NVARCHAR(50),
     cst_create_date DATE
 );
+CREATE OR REPLACE TABLE bronze.crm_sales_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+CREATE OR REPLACE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
+CREATE OR REPLACE TABLE bronze.erp_cust_az12 (
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+);
+CREATE OR REPLACE TABLE bronze.erp_px_cat_g1v2 (
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50)
+);
